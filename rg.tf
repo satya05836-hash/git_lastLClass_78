007eed787423f5577e3443bcbb636e68a1fd839a
satya05836-hash <satya05836@gmail.com>
2026-06-02 01:41:21 +0530

    Added VM configuration in feature branch

--- rg.tf
+++
@@ -1,5 +0,0 @@
-# 2. Resource Group बनाने का ब्लॉक
-resource "azurerm_resource_group" "my_rg" {
-  name     = "MyResourceGroup" # यहाँ अपने रिसोर्स ग्रुप का नाम लिखें
-  location = "East US"          # यहाँ अपनी मनपसंद लोकेशन/रीजन लिखें
-}
